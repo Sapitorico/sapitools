384f5505d3b9b8c690930ce8c1d95789566607d1
Sapitorico <renzoyaques@gmail.com>
2022-10-29 08:44:22 -0700

    update vimrc

---
+++ sapito.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    printf(" \n\n\n\n");
+    printf("       \\,/\n");
+    printf("     (`o_o')\n");
+    printf("    _(   )_\n");
+    printf("   /  .-.  \\\n");
+    printf("  (  /   \\  )\n");
+    printf("   \\ \\_/ \\_/\n");
+    printf("    \" ` \" ` \"\n");
+    printf(" \n\n\n\n");
+ 
+    return 0;
+}
